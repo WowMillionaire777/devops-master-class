de588138a77df5d656a156e497d1bb18f38e2e39
WowMillionaire777 <johnkenn777@outlook.com>
2022-07-18 18:19:07 -0400

    Updates starting terraform

---
+++ terraform/01-terraform-basics/main.tf
@@ -0,0 +1,29 @@
+# Configure the AWS Provider
+provider "aws" {
+    region = "us-east-1"
+    //version = "~> 2.46" (No longer necessary)
+}
+
+# plan - execute 
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-in28minutes-devops-002"
+    versioning {
+        enabled = true
+    }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc"
+}
+
+output "my_s3_bucket_versioning" {
+    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
+}
+
+output "my_s3_bucket_complete_details" {
+    value = aws_s3_bucket.my_s3_bucket
+}
+
+output "my_iam_user_complete_details" {
+    value = aws_iam_user.my_iam_user
+}
